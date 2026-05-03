d794a08e511f5a7b84f6d2733cb9874796b65487
Lucia Guillan <lguillan@frba.utn.edu.ar>
2026-05-03 19:24:23 -0300

    Revert "wip: experimento roto"
    This reverts commit 0281aa8ceebc37186b3627b2485a2daa78ab7ea8.

--- operaciones.c
+++ operaciones.c
@@ -14,7 +14,7 @@ int restar(int a, int b) {
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
-/*Recibe dos enteros y devuelve la multiplicacion entre ellos*/
+
 int multiplicar(int a, int b) {
     return a * b;
 }
